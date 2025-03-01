ca52fb72ba72216dfeff42154a1fc81d3ff12561
Fej1Dev <guymoshegg007@gmail.com>
2025-03-01 21:28:57 +0200

    better generics

--- src/main/java/com/fej1fun/ColoredNoiseMap.java
+++ src/main/java/com/fej1fun/ColoredNoiseMap.java
@@ -3,12 +3,12 @@ package com.fej1fun;
 import java.awt.image.BufferedImage;
 
 public class ColoredNoiseMap {
-    public static <T extends BiomeInterface<?>> BufferedImage getColorfulImage(Noise n, T biomeInterfaceClass) {
+    public static <T extends BiomeInterface<? extends Enum<?>>> BufferedImage getColorfulImage(Noise n, T biomeInterface) {
 
         BufferedImage image = new BufferedImage(n.width, n.height, BufferedImage.TYPE_INT_RGB);
         for (int y = 0; y < n.height; y++)
             for (int x = 0; x < n.width; x++)
-                image.setRGB(x, y, biomeInterfaceClass.getColorForNoisePoint(x, y, n, 0.2f).getRGB());
+                image.setRGB(x, y, biomeInterface.getColorForNoisePoint(x, y, n, 0.2f).getRGB());
 
         return image;
     }
